1f5c535e07475b910e155b415b9d6c8ba16837f3
Aaron Wislang <asw101@users.noreply.github.com>
2020-07-10 04:29:20 -0400

    Update __init__.py

--- 4-azure-functions-python-vscode/sentiment/__init__.py
+++ 4-azure-functions-python-vscode/sentiment/__init__.py
@@ -6,7 +6,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     analyzer = SentimentIntensityAnalyzer()
     text = req.params.get("text")
     if text is None:
-        return func.HttpResponse(status_code=302,headers={"Location":req.url+"?text=I+Love+PyCon"})
+        return func.HttpResponse(status_code=302, headers={"Location":req.url+"?text=I+Love+PyCon"})
     scores = analyzer.polarity_scores(text)
     sentiment = "positive" if scores["compound"] > 0 else "negative"
     return func.HttpResponse(sentiment)
