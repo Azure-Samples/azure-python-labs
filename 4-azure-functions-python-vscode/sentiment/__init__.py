795ae727d382394e4db3cb189282ab75089ca14b
Idan Bidani <iplaman@gmail.com>
2020-07-10 04:30:43 -0400

    Redirect if text query param missing (#20)
    * Redirect if text query param missing
    
    * Update __init__.py
    
    Co-authored-by: Aaron Wislang <asw101@users.noreply.github.com>

--- 4-azure-functions-python-vscode/sentiment/__init__.py
+++ 4-azure-functions-python-vscode/sentiment/__init__.py
@@ -5,6 +5,8 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 def main(req: func.HttpRequest) -> func.HttpResponse:
     analyzer = SentimentIntensityAnalyzer()
     text = req.params.get("text")
+    if text is None:
+        return func.HttpResponse(status_code=302, headers={"Location":req.url+"?text=I+Love+PyCon"})
     scores = analyzer.polarity_scores(text)
     sentiment = "positive" if scores["compound"] > 0 else "negative"
     return func.HttpResponse(sentiment)
