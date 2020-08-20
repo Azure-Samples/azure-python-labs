90c403b72a34291d6dbca3cecf4115152a8df2f6
Ruijun Hu (Wicresoft North America Ltd) <v-ruih@microsoft.com>
2020-08-20 16:41:49 +0800

    fix receiver idle time, use latest servicebus sdk

--- 8-azure-service-bus-messaging/app.py
+++ 8-azure-service-bus-messaging/app.py
@@ -12,8 +12,9 @@ if connection_string is None:
     print('ERROR: Requires `SB_CONNECTION` value to be set', file=sys.stderr)
     sys.exit(1)
 
-sb_client = ServiceBusClient.from_connection_string(os.environ['SB_CONNECTION'])
-queue_sender = sb_client.get_queue_sender('PyconLabQueue')
+queue_name='PyconLabQueue'
+sb_client = ServiceBusClient.from_connection_string(connection_string)
+queue_sender = sb_client.get_queue_sender(queue_name)
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -23,7 +24,7 @@ def send():
     message = request.get_data(cache=False, as_text=True)
     app.logger.info(f'SEND: {message}')    
     try:
-        queue_sender.send(Message(message))
+        queue_sender.send_messages(Message(message))
         success = True
     except MessageSendFailed:
         success = False
@@ -31,12 +32,12 @@ def send():
 
 @app.route('/receive', methods=['GET'])
 def get():
-    receiver = sb_client.get_queue_receiver('PyconLabQueue', idle_timeout=3)
-    messages = []
-    for message in receiver:
-        app.logger.info(f'Got message: {message}')
-        messages.append(str(message))
-        message.complete()
+    with sb_client.get_queue_receiver(queue_name, max_wait_time=3) as receiver:
+        messages = []
+        for message in receiver:
+            app.logger.info(f'Got message: {message}')
+            messages.append(str(message))
+            message.complete()
     return make_response(json.dumps(messages), {
         'Content-Type': 'application/json; charset=utf-8'
         })
