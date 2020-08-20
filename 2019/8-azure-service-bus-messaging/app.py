4755c9d2909b669da69c27a0d3cf4155eaf166a6
Ruijun Hu (Wicresoft North America Ltd) <v-ruih@microsoft.com>
2020-08-20 17:16:26 +0800

    update code & dependency with servicebus t2 sdk

--- 2019/8-azure-service-bus-messaging/app.py
+++ 2019/8-azure-service-bus-messaging/app.py
@@ -5,14 +5,16 @@ import logging
 
 from flask import Flask, request, make_response
 from azure.servicebus import ServiceBusClient, Message
+from azure.servicebus.exceptions import MessageSendFailed
 
 connection_string = os.environ.get('SB_CONNECTION')
 if connection_string is None:
     print('ERROR: Requires `SB_CONNECTION` value to be set', file=sys.stderr)
     sys.exit(1)
 
-sb_client = ServiceBusClient.from_connection_string(os.environ['SB_CONNECTION'])
-queue = sb_client.get_queue('PyconLabQueue')
+queue_name='PyconLabQueue'
+sb_client = ServiceBusClient.from_connection_string(connection_string)
+queue_sender = sb_client.get_queue_sender(queue_name)
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -20,18 +22,22 @@ app.logger.setLevel(logging.INFO)
 @app.route('/send', methods=['POST'])
 def send():
     message = request.get_data(cache=False, as_text=True)
-    app.logger.info(f'SEND: {message}')
-    (success, __) = queue.send(Message(message))[0]
+    app.logger.info(f'SEND: {message}')    
+    try:
+        queue_sender.send_messages(Message(message))
+        success = True
+    except MessageSendFailed:
+        success = False
     return str(success)
 
 @app.route('/receive', methods=['GET'])
 def get():
-    receiver = queue.get_receiver(idle_timeout=3)
-    messages = []
-    for message in receiver:
-        app.logger.info(f'Got message: {message}')
-        messages.append(str(message))
-        message.complete()
+    with sb_client.get_queue_receiver(queue_name, max_wait_time=3) as receiver:
+        messages = []
+        for message in receiver:
+            app.logger.info(f'Got message: {message}')
+            messages.append(str(message))
+            message.complete()
     return make_response(json.dumps(messages), {
         'Content-Type': 'application/json; charset=utf-8'
         })
