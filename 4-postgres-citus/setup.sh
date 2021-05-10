13e233a31f0ee977ddcb3d933fe841bbd77bc5e0
sudhanshuvishodia <41684987+sudhanshuvishodia@users.noreply.github.com>
2021-05-10 14:48:10 +0530

    Update setup.sh

--- 4-postgres-citus/setup.sh
+++ 4-postgres-citus/setup.sh
@@ -1,3 +1,4 @@
+# Download data files
 curl -O https://raw.githubusercontent.com/sudhanshuvishodia/azure-python-labs/patch-1/4-postgres-citus/data/area_reference.csv
 curl -O https://raw.githubusercontent.com/sudhanshuvishodia/azure-python-labs/patch-1/4-postgres-citus/data/metric_reference.csv
 curl -O https://raw.githubusercontent.com/sudhanshuvishodia/azure-python-labs/patch-1/4-postgres-citus/data/release_reference.csv
